9663dc2edbea2bea61cfaa273924e510d5e3565e
Will <will.cox@careerstep.com>
2019-04-05 17:01:44 -0600

    testing promises

--- node_contrast_modules/set-contrast-solution.js
+++ node_contrast_modules/set-contrast-solution.js
@@ -8,5 +8,15 @@ module.exports = function() {
 	var self = this;
 	global.__NodeContrastEntry__ = (args) => self.callStack.push(args);
 	global.__NodeContrastExit__ = (args) => self.callStack.pop(args);
+	global.__NodeContrastExit__ = async (args) => {
+		if (args && args.returnValue && !!args.returnValue.then) {
+			let rv = await args.returnValue;
+			console.log('here', args, rv)
+			return self.callStack.pop(args);
+		}
+		console.log('here2')
+		// console.log(typeof args.returnValue, args.returnValue, !!args.returnValue.then);
+		return self.callStack.pop(args)
+	};
 	global.__NodeContrastCatch__ = (args) => self.callStack.catch(args);
 };
